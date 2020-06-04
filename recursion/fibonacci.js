c609f7092bf9fc5dd13d52f0b11110c6093900da
Meghna Srivastava <meghna.s1906@gmail.com>
2020-06-04 19:36:13 +0200

    notes

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -1,26 +1,48 @@
-// ITERATIVE SOLUTION
+/* -----------------------------------------------
+ ITERATIVE SOLUTION
+----------------------------------------------- */
+function fibonacci(num) {
+  const result = [0, 1]; // [1, 1]
 
-function fib(n) {
-  const result = [0, 1]; // const result = [1, 1];
+  for (let i = 2; i <= num; i++) {
+    const last = result[result.length - 1];
+    const secondLast = result[result.length - 2];
 
-  for (let i = 2; i <= n; i++) {
-    const a = result[result.length - 1];
-    const b = result[result.length - 2];
-
-    result.push(a + b);
+    result.push(last + secondLast);
   }
-  console.log(result);
-  return result[n];
+  //   console.log(result);
+  return result[num];
 }
 
-// console.log(fib(19));
+console.log(fibonacci(6)); // 8
 
-// RECURSIVE SOLUTION
+/* -----------------------------------------------
+ RECURSIVE SOLUTION
+----------------------------------------------- */
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
 
-function fibonacci(n) {
-  if (n < 2) return n;
-
-  return fibonacci(n - 1) + fib(n - 2);
+  return fib(n - 1) + fib(n - 2);
 }
 
-console.log(fibonacci(19));
+// console.log(fib(6));
+
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+fib = memoize(fib);
+
+console.log(fib(6));
